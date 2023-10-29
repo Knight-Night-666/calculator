df73ca05d146574965b119033a2205013907d569
Knight-Night-666 <rprakhar14@gmail.com>
2023-10-30 02:11:31 +0530

    added functionality of Calc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,77 @@
 package org.example;
-// git token: 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
+import java.util.*;
+// git token: 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
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public float add(float a,float b){
+        return a+b;
+    }
+    public float sub(float a,float b){
+        return a-b;
+    }
+    public float div(float a,float b){
+        if(b!=0)
+            return a/b;
+        return -999999;
+    }
+    public float multiply(float a,float b){
+        return a*b;
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and not welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Main calc = new Main();
+        Scanner in = new Scanner(System.in);
+        System.out.println("WELCOME TO HELL, CHOOSE OPTIONS:");
+        int choice = 5;
+        do{
+            System.out.println("1: add");
+            System.out.println("2: subtract");
+            System.out.println("3: divide");
+            System.out.println("4: Multiply");
+            System.out.println("5: Exit");
+            choice = in.nextInt();
+            float a,b,ans;
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter 2 numbers");
+                    a = in.nextFloat();
+                    b = in.nextFloat();
+                    ans = calc.add(a, b);
+                    System.out.println("Apka Ans: " + ans);
+                    break;
+                case 2:
+                    System.out.println("Enter 2 numbers");
+                    a = in.nextFloat();
+                    b = in.nextFloat();
+                    ans = calc.sub(a, b);
+                    System.out.println("Apka Ans: " + ans);
+                    break;
+                case 3:
+                    System.out.println("Enter 2 numbers");
+                    a = in.nextFloat();
+                    b = in.nextFloat();
+                    ans = calc.div(a, b);
+                    if (ans != -999999)
+                        System.out.println("Apka Ans: " + ans);
+                    else
+                        System.out.println("Division by 0 error");
+                    break;
+                case 4:
+                    System.out.println("Enter 2 numbers");
+                    a = in.nextFloat();
+                    b = in.nextFloat();
+                    ans = calc.multiply(a, b);
+                    System.out.println("Apka Ans: " + ans);
+                    break;
+                case 5:
+                    System.out.println("Outta Hell");
+                    break;
+                default:
+                    System.out.println("Samajh ni ati kya choices");
+            }
+            System.out.println(" ");
+        }while(choice!=5);
+        in.close();
     }
 }
