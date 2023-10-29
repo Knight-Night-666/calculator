be92dc6fca38b94f58f8899cbea995ed73f878ea
Knight-Night-666 <rprakhar14@gmail.com>
2023-10-30 01:39:51 +0530

    test commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
-// git token: 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
+// git token: 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
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and not welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
